0042d068c81eca434250c5573aadc18fb0d73964
Jason Rylance <ecnalyr@gmail.com>
2012-12-20 16:27:01 -0600

    Changed double quotes to single quotes

--- scrape_sephora.py
+++ scrape_sephora.py
@@ -8,38 +8,38 @@ import json
 #  If we install pip and lxml correctly (gcc issues)
 #  This will allow us to use something that isn't a regular expression
 def soupify(htmlData):
-    '''Expects HTML data from a web page'''
+    """Expects HTML data from a web page"""
     return BeautifulSoup(htmlData, "html.parser")
 
 
 def getHtml(url):
-    '''Returns html data from a given url'''
+    """Returns html data from a given url"""
     return urllib2.urlopen(url)
 
 
 def stripTags(item):
-    '''Uses regex to remove xml tags from something
-    that can be turned into a string'''
+    """Uses regex to remove xml tags from something
+    that can be turned into a string"""
     stringVersion = str(item)
     return re.sub('<[^>]*>', '', stringVersion)  # i.e. <loc>
 
 
 def stripExtraSpaces(item):
-    '''Uses regex to remove extra spaces from something
-    that can be turned into a string'''
+    """Uses regex to remove extra spaces from something
+    that can be turned into a string"""
     stringVersion = str(item)
     return re.sub('  ', '', stringVersion)
 
 
 def stripNewLines(item):
-    '''Uses regex to remove new lines from something
-    that can be turned into a string'''
+    """Uses regex to remove new lines from something
+    that can be turned into a string"""
     stringVersion = str(item)
     return re.sub('\n', ' ', stringVersion)
 
 
 def getPriceFromLink(link):
-    '''Expects a string link to a sephora.com product page'''
+    """Expects a string link to a sephora.com product page"""
     soup = soupify(getHtml(link))
     try:
         price = soup.find("span", {"class": "price"}).string
@@ -49,8 +49,8 @@ def getPriceFromLink(link):
 
 
 def getPriceFromDiv(div):
-    '''Currently only returns FIRST PRICE - this is not the intended behavior'''
-    '''Expects a BeautifulSoup div from sephora.com new product page'''
+    """Currently only returns FIRST PRICE - this is not the intended behavior
+    Expects a BeautifulSoup div from sephora.com new product page"""
     try:
         price = div.find("span", {"class": "price"}).string
         return price
@@ -59,7 +59,7 @@ def getPriceFromDiv(div):
 
 
 def getBrandAndNameFromDiv(div):
-    '''Expects a BeautifulSoup div from sephora.com new product page'''
+    """Expects a BeautifulSoup div from sephora.com new product page"""
     try:
         brand = div.find("span", {"class": "name OneLinkNoTx"})
         return stripTags(stripExtraSpaces(stripNewLines(brand))).lstrip()
@@ -68,25 +68,25 @@ def getBrandAndNameFromDiv(div):
 
 
 def getLinkFromDiv(div):
-    '''Expects a BeautifulSoup div from sephora.com new product page'''
+    """Expects a BeautifulSoup div from sephora.com new product page"""
     try:
         return div.find('a', {'class': 'product-image'})['href']
     except AttributeError:
         return "There is no link"
 
 def getImageLinkFromDiv(div):
-    '''Expects a BeautifulSoup div from sephora.com new product page'''
+    """Expects a BeautifulSoup div from sephora.com new product page"""
     try:
         return div.find('a', {'class': 'product-image'}).find('img')['src']
     except AttributeError:
         return "There is no image link"
 
 def prependSephoraRoot(link):
-    '''Expects the suffix of a Sephora.com link'''
+    """Expects the suffix of a Sephora.com link"""
     return "http://www.sephora.com" + link
 
 def getSkuFromlink(link):
-    '''Expects the suffix of a Sephora.com product link'''
+    """Expects the suffix of a Sephora.com product link"""
     return re.search('P\d{6}', link).group(0)  # i.e. P123456
 
 def buildJsonPostRequest(url):
@@ -96,7 +96,7 @@ def buildJsonPostRequest(url):
     return request
 
 class SephoraProduct:
-    '''A product from Sephora crated using a newItemDiv in a BeautifulSoup format'''
+    """A product from Sephora crated using a newItemDiv in a BeautifulSoup format"""
     def __init__(self, newItemDiv):
         self.price = str(getPriceFromDiv(newItemDiv))
         self.brandAndName = str(getBrandAndNameFromDiv(newItemDiv))
@@ -104,7 +104,7 @@ class SephoraProduct:
         self.link = str(prependSephoraRoot(getLinkFromDiv(newItemDiv)))
         self.imageLink = str(prependSephoraRoot(getImageLinkFromDiv(newItemDiv)))
         self.sku = str(getSkuFromlink(self.link))
-        self.json = json.dumps({'name':self.brandAndName, 'price':self.price, 'sku':self.sku, 
+        self.json = json.dumps({'store':'Sephora', 'name':self.brandAndName, 'price':self.price, 'sku':self.sku, 
                                 'link':self.link, 'imageLink':self.imageLink, 'scrape_time':self.timeAdded})
 
     def getPrice(self):
@@ -139,7 +139,7 @@ for item in newItems:
     product = SephoraProduct(item)
     productList.append(product)
 
-for item in productList[0:2]:
+for item in productList[0:]:
     print str(item.getTimeAdded()) + " " + str(item.getPrice()) + " " + str(item.getBrandAndName())
     print item.getLink()
     print item.getImageLink()
