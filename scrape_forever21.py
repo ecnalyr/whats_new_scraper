36731d68bdb8de2152ef48065aff9b33a475f46c
Jason Rylance <ecnalyr@gmail.com>
2012-12-21 13:47:32 -0600

    Cleaned up comments

--- scrape_forever21.py
+++ scrape_forever21.py
@@ -6,28 +6,6 @@ from scraper_tools import soupify, getHtml, buildJsonPostRequest
 __author__ = 'ecnalyr'
 
 
-uploadProductsUrl = "http://0.0.0.0:3000/products.json"
-forever21NewItemLink = "http://www.forever21.com/Product/Category.aspx?br=f21&category=whatsnew_all&pagesize=100&page=1"
-newItemSoup = soupify(getHtml(forever21NewItemLink))
-newItems = newItemSoup.find_all("table", style="float: right")
-productBaseUrl = "http://www.forever21.com/Product/Product.aspx?BR=f21&Category=whatsnew_all&ProductID="
-# Can look for a table who's style is "height:300px;float: right" - this is a product
-# each product table contains two trs: the first eventually contains the image, the second contains name/price
-# The image can be found Product Table > first img tag src
-# The brand can be found Product Table > second tr > div class="ItemTag2" gives the brand
-# The name can be found Product Table > second tr > first <a> > div class="DisplayName"
-# The price can be found Product Table > second tr > font class="price" - the price will have a dollar sign
-# The SKU can be found Product Table > second tr > first input field's value="" < SKU will be there
-# The link can be built with this format: "http://www.forever21.com/Product/Product.aspx?BR=f21&Category=whatsnew_all&ProductID=" + SKU
-# Here's a product:
-#<table border="0" height="300" style="float: right">
-#<tr>
-#<td align="center" class="td_col_b01" height="292" id="ctl00_MainContent_dlCategoryList_ctl00_tdImageColumn" valign="bottom"><div class="ItemImage"><a href="http://www.forever21.com/Product/Product.aspx?BR=f21&amp;Category=whatsnew_all&amp;ProductID=2051923777&amp;VariantID="><img border="0" id="image_2051923777" onerror="imgError(this);" onmouseout='fnChangeProductImageForMouseEvent(this, "http://www.forever21.com/images/main_cat/51923777-02.jpg");' onmouseover='fnChangeProductImageForMouseEvent(this, "http://www.forever21.com/images/main_cat/51923777-02.jpg");' src="http://www.forever21.com/images/main_cat/51923777-02.jpg"/></a><a class="play" href="javascript:showPopWin('/Product/product_pop.aspx?BR=f21&amp;Category=whatsnew_all&amp;ProductID=2051923777&amp;VariantID=',730,550,null,'center');"><img src="http://www.forever21.com/images/en/quickview/btn_quickview.png"/></a></div></td>
-#</tr><tr>
-#<td align="left" class="td_col_b01" height="90" id="ctl00_MainContent_dlCategoryList_ctl00_tdValueColumn" valign="top" width="199"><span id="ctl00_MainContent_dlCategoryList_ctl00_dvContainer"><div class="ItemTag1"><img src="http://www.forever21.com/images/en/tag/web_exclusive_199.gif"/></div><div class="ItemTag2">CAPSULE 2.1</div><a href="http://www.forever21.com/Product/Product.aspx?BR=f21&amp;Category=whatsnew_all&amp;ProductID=2051923777&amp;VariantID="><div class="DisplayName">Cross Stitch Maxi Skirt</div></a><font class="price">$29.80</font><br/><input id="ctl00_MainContent_dlCategoryList_ctl00_hdProductId" name="ctl00$MainContent$dlCategoryList$ctl00$hdProductId" type="hidden" value="2051923777"/><input id="ctl00_MainContent_dlCategoryList_ctl00_hdProductSKUId" name="ctl00$MainContent$dlCategoryList$ctl00$hdProductSKUId" type="hidden"/><input id="ctl00_MainContent_dlCategoryList_ctl00_hdColorCode" name="ctl00$MainContent$dlCategoryList$ctl00$hdColorCode" type="hidden"/></span></td>
-#</tr>
-#</table>
-
 def getImageLinkFromTable(table):
     """Expects a BeautifulSoup table from forever21.com new product page"""
     try:
@@ -101,13 +79,17 @@ class Forever21Product:
         return self.imageLink
 
     def getSku(self):
-        '''Note that this is different than Sephora's item numbers'''
+        '''Note that this is different than Forever 21's item numbers'''
         return self.sku
 
     def getJson(self):
         return self.json
 
-
+uploadProductsUrl = "http://0.0.0.0:3000/products.json"
+forever21NewItemLink = "http://www.forever21.com/Product/Category.aspx?br=f21&category=whatsnew_all&pagesize=100&page=1"
+newItemSoup = soupify(getHtml(forever21NewItemLink))
+newItems = newItemSoup.find_all("table", style="float: right")
+productBaseUrl = "http://www.forever21.com/Product/Product.aspx?BR=f21&Category=whatsnew_all&ProductID="
 
 productList = []
 for item in newItems:
